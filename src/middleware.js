d724a50829908d2129cbc4c1dfa5c6be7dc78641
apoorvGit <apoorvmishra1000@gmail.com>
2023-02-03 16:05:28 +0530

    feat: add middleware validator for update ceo request

--- src/middleware.js
+++ src/middleware.js
@@ -8,6 +8,10 @@ const idValidatorSchema=joi.object({
     id: joi.string().regex(/^[a-zA-Z0-9\s]+$/).required()
 })
 
+const idAndStringValidatorSchema=joi.object({
+    "id": joi.string(),
+    "ceo": joi.string()
+})
 //validators
 const postValidator=(req,res,next)=>{
     const {error}=postValidatorSchema.validate(req.body)
@@ -23,5 +27,13 @@ const idValidator=(req,res,next)=>{
     }
     next()
 }
-
-module.exports={postValidator, idValidator}
+const idAndStringValidator=(req,res,next)=>{
+    console.log(req.params.id)
+    console.log(req.body.ceo)
+    const {error}=idAndStringValidatorSchema.validate({"id":req.params.id, "ceo": req.body.ceo} )
+    if(error){
+        return res.status(400).json({"message":error.message})
+    }
+    next()
+}
+module.exports={postValidator, idValidator, idAndStringValidator}
