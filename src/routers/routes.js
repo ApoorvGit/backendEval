73b01d9419b9b7c38913dd47becdbedd33ccbf22
apoorvGit <apoorvmishra1000@gmail.com>
2023-02-03 16:04:52 +0530

    feat: add router for update ceo request

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express()
-const {postUrl, getCompanies} = require("../controllers/controller.js")
+const {postUrl, getCompanies, updateCeo} = require("../controllers/controller.js")
 const middleware=require("../middleware")
 router.use(express.json())
 router.post("/save",middleware.postValidator,postUrl)
 router.get("/companies/sector/:id", middleware.idValidator, getCompanies)
+router.patch("/company/ceo/update/:id", middleware.idAndStringValidator, updateCeo)
 module.exports=router
